cc7294a85adac43cce99160602e9c9d21c41ac6d
Jparellada79 <jparellada2000@hotmail.com>
2022-04-09 20:13:51 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const fotom = document.getElementById("fotom");
+const fotos = document.getElementById("fotosa");
+const fotor = document.getElementById("fotor");
+const fotop = document.getElementById("fotop");
+
+function cambiaportada(id) {
+
+    let src = id.src;
+    const m = src.split("/")
+    const s = parseInt(m.pop().substring(1, 2));
+    let k = 0;
+    console.log(src);
+
+    if (s == 3) {
+        k = 0;
+    } else {
+        k = s + 1;
+    }
+    src = src.replace(`${ s.toString() }.jpg`, `${ k.toString() }.jpg`);
+    id.src = src;
+
+
+}
+fotom.addEventListener('click', () => {
+    cambiaportada(fotom)
+})
+fotos.addEventListener('click', () => {
+    cambiaportada(fotos)
+})
+
+fotor.addEventListener('click', () => {
+    cambiaportada(fotor)
+})
+
+fotop.addEventListener('click', () => {
+    cambiaportada(fotop)
+})
